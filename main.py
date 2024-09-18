2d370f2a3b7c84d698b23daf944279497e6e09b1
Mang00o <soci.alex24@gmail.com>
2024-09-18 08:53:36 +0200

    Refactor main: introduction Class Retriving

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 from prittier import frame_text
 from warning import manage_warning
 from retriving import Retriving
+from embedding import Embedding
 
 def main():
     frame_text('Inizio dello Script')
 
     manage_warning()
 
-    #########################
-    #       RETRIVING       #
-    #########################
+    #################################
+    #       PHASE 1 ~ RETRIVING     #
+    #################################
+
     # Specifica la directory dove si trovano i PDF
     directory = "documents"
 
     # Crea un'istanza di Retriving
     retriving = Retriving(directory)
 
-    # Estrai il testo dai PDF e stampa i nomi dei documenti caricati
-    
-    retriving.print_loaded_documents()
-    # Carica i documenti PDF e salva i nomi
+    # Estre testo da pdf nella directory
     pdf_texts, pdf_filenames = retriving.extract_text_from_pdfs()
 
+    # Stampa i nomi dei documenti pdf da cui ha estratto il testo
+    retriving.print_loaded_documents()
+
+    
     
 
     # Stampa fine script
