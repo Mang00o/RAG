6d79b71530d18eb143cbfb22e47362531426af2d
Mang00o <soci.alex24@gmail.com>
2024-09-18 11:37:58 +0200

    Fix Retriving.print: add self

--- retrieving.py
+++ retrieving.py
@@ -1,3 +1,5 @@
+from embedding import Embedding
+
 class Retrieving:
     def __init__(self, document_embeddings, pdf_filenames, index):
         # Save document embeddings, file names, FAISS index
@@ -10,14 +12,18 @@ class Retrieving:
 
     # Function to search for relevant documents based on a query
     def search_documents(self, k=1):
+        embedding = Embedding()
+        
+        query_embedding = embedding.embed_documents([self.query])
+        
         # Search the FAISS index for the documents closest to the query embedding
-        distances, indices = self.index.search(self.query, k)
+        distances, indices = self.index.search(query_embedding, k)
 
         # Retrieve the most relevant documents and their distances
         results = [(self.pdf_filenames[i], distances[0][idx]) for idx, i in enumerate(indices[0])]
         return results
     
-    def print_relevant_documents(result):
+    def print_relevant_documents(self, result):
         # Print the header for the relevant documents
         print("\n-> Most relevant documents found:")
         print("\tDocument                                | Distance")
