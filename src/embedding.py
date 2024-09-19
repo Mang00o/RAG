7941d9705fe7dd63a203c44b55f614122bd1c13f
Mang00o <soci.alex24@gmail.com>
2024-09-19 09:35:46 +0200

    Up Embedding: add normalizaziont

--- src/embedding.py
+++ src/embedding.py
@@ -1,3 +1,4 @@
+import faiss
 from transformers import AutoTokenizer, AutoModel
 import torch
 
@@ -7,11 +8,13 @@ class Embedding:
         self.tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
         self.model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
 
-    # Method for transforming document text into vectors
+    # Method for transforming document text into vectors and normalizing them
     def embed_texts(self, texts):
         inputs = self.tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
         with torch.no_grad():
             embeddings = self.model(**inputs).last_hidden_state.mean(dim=1).numpy()
-        return embeddings
 
-        
+        # Normalize the embeddings (L2 normalization)
+        faiss.normalize_L2(embeddings)
+
+        return embeddings
