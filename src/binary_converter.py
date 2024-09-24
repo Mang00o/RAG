1f8dfaa366dfdfefdf21a1de1aa0d168004b2d1b
Mang00o <soci.alex24@gmail.com>
2024-09-24 14:49:31 +0200

    Add BinaryConverter

---
+++ src/binary_converter.py
@@ -0,0 +1,14 @@
+import pickle
+
+class BinaryConverter:
+    # Method for returning the binary text of a text
+    def binary_embedding(self, text):
+        # Convert text to binary 
+        binary_text = pickle.dumps(text)
+        return binary_text
+    
+    # Method for converting binary text back to normal text
+    def normal_text(self, binary_text):
+        # Convert binary text back to normal text
+        text = pickle.loads(binary_text)
+        return text
