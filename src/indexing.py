5274a8e9a7899b0f327b1c1245f045275488e299
Mang00o <soci.alex24@gmail.com>
2024-09-26 12:26:35 +0200

    RE IN: var name

--- src/indexing.py
+++ src/indexing.py
@@ -5,8 +5,8 @@ class Indexing:
         # Initialize a FAISS index using L2 (Euclidean) distance
         self.indexes = faiss.IndexFlatL2(embedding_dimension)
 
-    def add(self, embed_documents):
+    def add(self, embedded_contents):
         # Add the document embeddings to the FAISS index
-        self.indexes.add(embed_documents)
+        self.indexes.add(embedded_contents)
         return self.indexes
     
