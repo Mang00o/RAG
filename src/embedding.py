4b766867ea48d3ff591dd80bba5e466be245d2a8
Mang00o <soci.alex24@gmail.com>
2024-09-26 11:34:32 +0200

    RE EM: var names + comments

--- src/embedding.py
+++ src/embedding.py
@@ -8,9 +8,9 @@ class Embedding:
         self.model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
 
     # Method for transforming document text into vectors and normalizing them
-    def embedding(self, text):
+    def embedding(self, ingested_documentes_texts):
         # Tokenize the texts
-        inputs = self.tokenizer(text, padding=True, truncation=True, return_tensors="pt")
+        inputs = self.tokenizer(ingested_documentes_texts, padding=True, truncation=True, return_tensors="pt")
         
         # Compute the embeddings without gradient calculation
         with torch.no_grad():
@@ -20,3 +20,13 @@ class Embedding:
         faiss.normalize_L2(embeddings)
 
         return embeddings
+        # Example of the embedding array:
+        # Each row represents the embedding of a single text, and each value is a dimension of the embedding.
+        # The array has a shape of (num_texts, embedding_dimension).
+        # For example, with 3 texts and an embedding dimension of 384, it might look like this:
+
+        # array([
+        #     [0.12, 0.34, ..., 0.45],  # Embedding of the first text
+        #     [0.22, 0.56, ..., 0.34],  # Embedding of the second text
+        #     [0.54, 0.23, ..., 0.67],  # Embedding of the third text
+        # ])
