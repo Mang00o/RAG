872a78282cfa1c0cf1f3f075ec67b401d9bf0c86
Mang00o <soci.alex24@gmail.com>
2024-10-01 12:42:32 +0200

    RE EMB: better initializing

--- src/embedding.py
+++ src/embedding.py
@@ -1,16 +1,22 @@
 import faiss
 from transformers import AutoTokenizer, AutoModel
 import torch
+
+#################################################################
+#   This class compute the embedding of the ingested texts.     #
+#   Return the normalized embeddings of that texts              #
+#################################################################
 class Embedding:
-    def __init__(self):
+    def __init__(self,ingested_documentes_texts):
+        self.ingested_documentes_texts = ingested_documentes_texts
         # Load the embedding model and tokenizer
         self.tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
         self.model = AutoModel.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
 
     # Method for transforming document text into vectors and normalizing them
-    def embedding(self, ingested_documentes_texts):
+    def embedding(self):
         # Tokenize the texts
-        inputs = self.tokenizer(ingested_documentes_texts, padding=True, truncation=True, return_tensors="pt")
+        inputs = self.tokenizer(self.ingested_documentes_texts, padding=True, truncation=True, return_tensors="pt")
         
         # Compute the embeddings without gradient calculation
         with torch.no_grad():
@@ -20,13 +26,3 @@ class Embedding:
         faiss.normalize_L2(embeddings)
 
         return embeddings
-        # Example of the embedding array:
-        # Each row represents the embedding of a single text, and each value is a dimension of the embedding.
-        # The array has a shape of (num_texts, embedding_dimension).
-        # For example, with 3 texts and an embedding dimension of 384, it might look like this:
-
-        # array([
-        #     [0.12, 0.34, ..., 0.45],  # Embedding of the first text
-        #     [0.22, 0.56, ..., 0.34],  # Embedding of the second text
-        #     [0.54, 0.23, ..., 0.67],  # Embedding of the third text
-        # ])
