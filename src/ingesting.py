d9c64a8282044777f71675f9091ea344c76955be
Mang00o <soci.alex24@gmail.com>
2024-10-01 14:59:20 +0200

    RE ING: better structure

--- src/ingesting.py
+++ src/ingesting.py
@@ -1,39 +1,52 @@
 import os
 import PyPDF2
 
+#################################################################################
+#   This class deals with ingesting the PDFs contained in a specific folder.    #
+#   The class checks that the PDFs have not already been saved.                 #
+#   Finally it returns the name and content of each unsaved PDF                 #
+#################################################################################
 class Ingesting:
-    def __init__(self, directory):
-        self.directory = directory
-        self.documents = []
-        self.document_names = []
+    def __init__(self, document_directory, db_pdfs_names):
+        self.document_directory = document_directory
+        self.pdfs_names = []
+        self.pdfs_contents = []
+        self.db_pdfs_names = db_pdfs_names
+
+    # Main function of the class - Ingest documents
+    def ingesting(self):
+        self.__ingesting_pdfs_names()
+        self.__ingesting_pdfs_contents()
+        return self.pdfs_names, self.pdfs_contents
 
     # Function to get the PDF filenames
-    def get_pdf_filenames(self):
-        for filename in os.listdir(self.directory):
-            if filename.endswith(".pdf"):
-                self.document_names.append(filename)
-        return self.document_names
+    def __ingesting_pdfs_names(self):    
+        for pdf_name in os.listdir(self.document_directory):
+            if pdf_name.endswith(".pdf") and pdf_name not in self.db_pdfs_names:
+                self.pdfs_names.append(pdf_name)
+        
+    def __ingesting_pdfs_contents(self):
+        for pdf_name in self.pdfs_names:
+            content = self.__extract_content_from_pdf(pdf_name)
+            self.pdfs_contents.append(content)
 
+    # Function to extract text from a single PDF with improvements
+    def __extract_content_from_pdf(self, pdf_name):
+        content = []  # Use a list to accumulate the content for better performance
+        try:
+            with open(os.path.join(self.document_directory, pdf_name), 'rb') as pdf:
+                pdf_reader = PyPDF2.PdfReader(pdf)
+                
+                for page in range(len(pdf_reader.pages)):
+                    page_text = pdf_reader.pages[page].extract_text()
+                    
+                    # Check if text was extracted from the page
+                    if page_text:
+                        content.append(page_text)
+            
+            # Join the list of text into a single string
+            return "".join(content)
 
-    # Function to extract text from a single PDF
-    def extract_text_from_pdf(self, filename):
-        with open(os.path.join(self.directory, filename), 'rb') as file:
-            reader = PyPDF2.PdfReader(file)
-            text = ""
-            for page in range(len(reader.pages)):
-                text += reader.pages[page].extract_text()
-        return text
-
-    # Main function that uses the above two functions to process all PDFs
-    def extract_text_from_pdfs(self):
-        pdf_filenames = self.get_pdf_filenames()
-        for filename in pdf_filenames:
-            text = self.extract_text_from_pdf(filename)
-            self.documents.append(text)
-        return self.documents, self.document_names
-
-    # Method for printing the names of uploaded files
-    def print_loaded_documents(self):
-        print("\n-> Load Files:")
-        for filename in self.document_names:
-            print(f"\t- {filename}")
+        except Exception as e:
+            print(f"\n-> Error reading {pdf_name}: {e}")
+            return ""
